4d9f775b65b280bba6c93fbbd0e2c39c1dd04b0a
swati30v <swati30v@gmail.com>
2020-04-13 17:51:11 +0530

    complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { EditprofileComponent } from './editprofile/editprofile.component';
 import { AuthService } from './auth.service';
 import { AuthGuard } from './auth.guard';
 
@@ -11,7 +12,9 @@ const routes: Routes = [
   {path:"",component: SignupComponent},
   { path:"login",component: LoginComponent},
   { path:"home",pathMatch: 'full', canActivate: [AuthGuard],component: HomeComponent},
+  { path:"editprofile",component: EditprofileComponent},
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(
